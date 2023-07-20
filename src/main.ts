c859a1d0f32ae12344846442b7df6c1aa50698ce
dani <danicalbet@gmail.com>
2023-07-20 12:12:35 +0200

    including materialize styles, and including tab compo

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import './style.css'
+import './style.scss'
+import { M } from "@materializecss/materialize"
+
 import { setupCounter } from './counter.ts'
 
 
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+M.Tabs.init(document.querySelectorAll('.tabs'), {});
+M.Tabs.init(document.querySelectorAll('#tabs-swipe-demo'), {swipeable: true});
